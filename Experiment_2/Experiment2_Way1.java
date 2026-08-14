f6f38f132acf27cff0b4598c941089b024d644f4
hrshtyagi <harshsonepat@gmail.com>
2026-08-14 10:43:33 +0530

    Create Experiment2_Way1.java

---
+++ Experiment_2/Experiment2_Way1.java
@@ -0,0 +1,27 @@
+public class ContainsDuplicateIIBruteForce {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+                    return true;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+}
